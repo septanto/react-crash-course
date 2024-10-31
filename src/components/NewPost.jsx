1dad8d74b35c9744e16c3329371ac011a379f388
septanto <s.s.adi.nugroho@gmail.com>
2024-10-31 17:12:35 +0700

    useEffect

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,7 +1,7 @@
 import classes from './NewPost.module.css';
 import { useState } from 'react';
 
-function NewPost({ onCancel }) {
+function NewPost({ onCancel, onAddPost }) {
 
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
@@ -20,7 +20,7 @@ function NewPost({ onCancel }) {
             body: enteredBody,
             author: enteredAuthor
         }
-        console.log(postData);
+        onAddPost(postData)
         onCancel();
     }
 
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
@@ -5,19 +6,53 @@ import classes from './PostsList.module.css'
 
 function PostsList({ isPosting, onStopPosting }) {
 
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts')
+            const resData = await response.json();
+            setPosts(resData.posts);
+        }
+
+        fetchPosts();
+    }, []);
+
+    function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        } );
+        setPosts((existingPosts) => [postData, ...existingPosts])
+    }
+
     return (
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
                     <NewPost
                         onCancel={onStopPosting}
+                        onAddPost={addPostHandler}
                     />
                 </Modal>
             )}
 
-            <ul className={classes.posts}>
-                <Post />
-            </ul>
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />
+                    )}
+                </ul>)}
+
+            {posts.length === 0 && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <h2>There are no posts yet</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
+
         </>
     )
 }
