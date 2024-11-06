5ac604bf15758e8507e29ade7098375e4db5d663
septanto <s.s.adi.nugroho@gmail.com>
2024-11-06 11:47:13 +0700

    init routing & rendering

--- app/meals/[mealSlug]/not-found.js
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <main className="not-found">
-            <h1>Not Found</h1>
-            <p>Unfortunately, we could not find the requested meal data</p>
-        </main>
-    )
-}
