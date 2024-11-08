1c94e8b786de1f5aeb083921e15e6e035f99a705
septanto <s.s.adi.nugroho@gmail.com>
2024-11-08 22:41:00 +0700

    read db

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,49 +1,12 @@
-'use client';
-
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
+import { getAllNews } from "@/lib/news";
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
-
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/news')
-
-      if (!response.ok) {
-        setError('Failed to fetch news.')
-        setIsLoading(false);
-      }
-
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>
-  }
-
-  if (error) {
-    return <p>{error}</p>
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsList news={news} />
-  }
-
+export default async function NewsPage() {
+  const news = getAllNews();
   return (
     <div id="news">
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </div>
   );
 }
