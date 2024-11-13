c75c299d9b2f756d841018e368f61563f741c8b7
septanto <s.s.adi.nugroho@gmail.com>
2024-11-13 10:06:17 +0700

    metadata

--- components/header.js
+++ components/header.js
@@ -1,11 +1,18 @@
 import logo from '@/assets/logo.png';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          alt="Mobile phone with posts feed on it"
+          priority
+        />
       </Link>
       <nav>
         <ul>
