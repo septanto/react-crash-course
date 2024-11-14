ff5f43b884e616614053a5b6a6230909339542e6
septanto <s.s.adi.nugroho@gmail.com>
2024-11-14 14:40:05 +0700

    login

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,28 +1,78 @@
-'use server';
+"use server";
 
-import { hashUserPassword } from "@/lib/hash";
-import { createUser } from "@/lib/user";
+import { createAuthSession } from "@/lib/auth";
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+import { createUser, getUserByEmail } from "@/lib/user";
+import { redirect } from "next/navigation";
 
 export async function signup(prevState, formData) {
-    const email = formData.get('email');
-    const password = formData.get('password');
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    let errors = {};
+  let errors = {};
 
-    if (!email.includes('@')) {
-        errors.email = 'Please enter a valid email address.';
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password need to be 8 chars or more";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+
+  const hashedPassword = hashUserPassword(password);
+  try {
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect("/training");
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email:
+            "It seems like an account for the chosen email already exists.",
+        },
+      };
     }
+    throw error;
+  }
+}
 
-    if (password.trim().length < 8) {
-        errors.password = 'Password need to be 8 chars or more';
-    }
+export async function login(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    if (Object.keys(errors).length > 0) {
-        return {
-            errors,
-        }
-    }
+  const existingUser = getUserByEmail(email);
+  if (!existingUser) {
+    return {
+      errors: {
+        email: "Could not authenticate user, please check your emails.",
+      },
+    };
+  }
 
-    const hashedPassword = hashUserPassword(password);
-    createUser(email, hashedPassword);
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+      errors: {
+        password: "Could not authenticate user, please check your credentials.",
+      },
+    };
+  }
+
+  await createAuthSession(existingUser.id);
+  redirect("/training");
+}
+
+export async function auth(mode, prevState, formData) {
+  if (mode === "login") {
+    return login(prevState, formData);
+  }
+  return signup(prevState, formData);
 }
